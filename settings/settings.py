e45b2b24a1f645f5bf0126eb3e05f16b2df4d3e6
Erik Fernando <erikfernandu@gmail.com>
2023-11-30 10:27:36 -0300

    Envio dos migrates

--- settings/settings.py
+++ settings/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'configuracao',
     'cadastro_pessoa',
     'produto',
-    'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
